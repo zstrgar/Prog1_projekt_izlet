23fae096e43de56e23546f19c9cae1971ae4b795
Zana Strgar <zstrgar@gmail.com>
2018-10-31 20:14:39 +0100

    vzorec posameznega izleta

--- izlet.py
+++ izlet.py
@@ -3,11 +3,11 @@ import re
 import os
 import csv
 
-url = 'http://www.hribi.net/goreiskanjerezultat.asp?drzavaid=1&gorovjeid=&goraime=&VisinaMIN=&VisinaMAX=&CasMIN=&CasMAX=&izhodisce=&izhodisceMIN=&izhodisceMAX=&VisinskaRazlikaMIN=&VisinskaRazlikaMAX=&zahtevnostid=&zahtevnostSmucanjeid=&IzhodisceMinOddaljenost=&IzhodisceMAXOddaljenost=&GoraMinOddaljenost=&GoraMaxOddaljenost=&mojaSirina=0&mojaDolzina=0'
+url_izletov = 'http://www.hribi.net/goreiskanjerezultat.asp?drzavaid=1&gorovjeid=&goraime=&VisinaMIN=&VisinaMAX=&CasMIN=&CasMAX=&izhodisce=&izhodisceMIN=&izhodisceMAX=&VisinskaRazlikaMIN=&VisinskaRazlikaMAX=&zahtevnostid=&zahtevnostSmucanjeid=&IzhodisceMinOddaljenost=&IzhodisceMAXOddaljenost=&GoraMinOddaljenost=&GoraMaxOddaljenost=&mojaSirina=0&mojaDolzina=0'
 #mapa s podatki
-directory = 'Prog1_pariski_hoteli/data'
+directory = 'Prog1_pariski_hoteli/podatki_izlet'
 #ime datoteke, kjer bomo shranili spletno stran
-filename = 'spletna_stran.html'
+filename = 'spletna_stran_izletov.html'
 #ime CSV datoteke
 csv = 'podatki.csv'
 
@@ -24,14 +24,14 @@ def download_url_to_string(url):
     return r.text
 
 
-def save_page():
+def save_page(url):
     ''' Pomožna funkcija, ki shrani url stran
     kot niz v datoteko v neki mapi.'''
     besedilo = download_url_to_string(url)
     os.makedirs(directory, exist_ok=True)
     path = os.path.join(directory, filename)
     with open(path, 'w', encoding='utf-8') as datoteka:
-        datoteka.write(besedilo)        
+        datoteka.write(besedilo)       
     return None
 
 # Obdelava podatkov
@@ -45,10 +45,21 @@ def read_file_to_string(directory, filename):
 
 vzorec = re.compile(
     r'<td colspan="2"><a href="(?P<Povezava>.+?)"><b>(?P<Ime>.+?)&nbsp;.+?',
-    flags = re.DOTALL
+    flags=re.DOTALL
 )
 
 
 def page_to_izleti(page):
     izleti = re.findall(vzorec, page)
     return izleti
+
+
+vzorec_izleta = re.compile(
+    r'<tr><td><b>Gorovje:</b> <a class="moder" href=.*?>(?P<Gorovje>.*?)</a></td></tr>.*?'
+    r'</b> (?P<Nadmorska_visina>\d{1,4})&nbsp;m</td></tr>*?'
+    r'<tr><td><b>Priljubljenost:</b> (?P<Priljubljenost>\d{1,3})%.*?'
+    r'(?P<Mesto_priljubljenosti>.\d{1,4})\.mesto.</td></tr>.*?',
+    #r'poti:</b> <a class="moder" href="#poti">(?P<Stevilo_poti>\d*?)</a></td></tr>.*?'
+    #r'<tr><td><b>Vrsta:</b> (?P<vrh>.?*)</td></tr>.?*',
+    flags=re.DOTALL
+)
