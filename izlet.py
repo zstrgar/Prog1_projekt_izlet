e3deb7acb389ba7606c3529a1da6831a9e1c0ba5
Zana Strgar <zstrgar@gmail.com>
2018-11-02 21:25:48 +0100

    Dodani parametri pri vzorcih

--- izlet.py
+++ izlet.py
@@ -5,11 +5,11 @@ import csv
 
 url_izletov = 'http://www.hribi.net/goreiskanjerezultat.asp?drzavaid=1&gorovjeid=&goraime=&VisinaMIN=&VisinaMAX=&CasMIN=&CasMAX=&izhodisce=&izhodisceMIN=&izhodisceMAX=&VisinskaRazlikaMIN=&VisinskaRazlikaMAX=&zahtevnostid=&zahtevnostSmucanjeid=&IzhodisceMinOddaljenost=&IzhodisceMAXOddaljenost=&GoraMinOddaljenost=&GoraMaxOddaljenost=&mojaSirina=0&mojaDolzina=0'
 #mapa s podatki
-directory = 'Prog1_projekt_izlet/podatki_izlet'
+ime_mape = 'Prog1_projekt_izlet/podatki_izlet'
 #ime datoteke, kjer bomo shranili spletno stran
-filename = 'spletna_stran_izletov.html'
+ime_datoteke = 'spletna_stran_izletov.html'
 #ime CSV datoteke
-csv = 'podatki.csv'
+csv_izletov = 'podatki.csv'
 
 
 def pripravi_imenik(ime_datoteke):
@@ -32,55 +32,60 @@ def download_url_to_string(url):
     return r.text
 
 
-def save_page(url, directory, filename):
-    ''' Pomožna funkcija, ki shrani url stran
-    kot niz v datoteko v neki mapi.'''
+def save_page(url, ime_mape, ime_datoteke):
+    ''' Pomožna funkcija, ki shrani url stran v dani 
+    mapi kot niz v datoteko.'''
     besedilo = download_url_to_string(url)
-    os.makedirs(directory, exist_ok=True)
-    path = os.path.join(directory, filename)
+    os.makedirs(ime_mape, exist_ok=True)
+    path = os.path.join(ime_mape, ime_datoteke)
     with open(path, 'w', encoding='utf-8') as datoteka:
         datoteka.write(besedilo)       
     return None
 
 
 # Obdelava podatkov
-def read_file_to_string(directory, filename):
+def read_file_to_string(ime_mape, ime_datoteke):
     ''' Vrne vsebino strani iz datoteke, kjer je shranjena, kot niz. '''
-    path = os.path.join(directory, filename)
+    path = os.path.join(ime_mape, ime_datoteke)
     with open(path, 'r', encoding='utf-8') as file_in:
         return file_in.read()
     return None
 
 
-vzorec = re.compile(
-    r'<td colspan="2"><a href="(?P<Povezava>.+?)"><b>(?P<Ime>.+?)&nbsp;.+?',
+vzorec_izletov = re.compile(
+    r'<td colspan="2"><a href="(?P<Povezava>gora\.asp\?gorovjeid=\d+?&id=(?P<id_izleta>\d+?))">'
+    r'<b>(?P<Ime>.+?)&nbsp;.+?',
     flags=re.DOTALL
 )
 
 
-def page_to_izleti(page):
-    izleti = re.findall(vzorec, page)
+def page_to_izleti(stran):
+    '''Vrne seznam naborov (url izleta, id izleta, ime izleta) iz strani.'''
+    izleti = re.findall(vzorec_izletov, stran)
     return izleti
 
 
 vzorec_izleta = re.compile(
+    r'gorast\((?P<id_izleta>\d+?)\);.*?'
+    r'<title>(?P<Ime_izleta>.+?)</title>\s.*?'
     r'<tr><td><b>Gorovje:</b> <a class="moder" href=.*?>(?P<Gorovje>.*?)</a></td></tr>.*?'
-    r'</b> (?P<Nadmorska_visina>\d{1,4})&nbsp;m</td></tr>.*?'
-    r'<tr><td><b>Vrsta:</b> (?P<vrsta>.*?)</td></tr>.*?'
+    r'</b> (?P<Visina>\d{1,4})&nbsp;m</td></tr>.*?'
+    r'<tr><td><b>Vrsta:</b> (?P<Vrsta>.*?)</td></tr>.*?'
     r'<tr><td><b>Priljubljenost:</b> (?P<Priljubljenost>\d{1,3}%).*?'
-    r'\((?P<mesto>\d{1,4}\. mesto)\)</td></tr>.*?'
-    r'href="#poti">(?P<St_poti>\d)</a></td></tr>.*?',
+    r'\((?P<Mesto_priljubljenosti>\d{1,4}\. mesto)\)</td></tr>.*?'
+    r'href="#poti">(?P<Stevilo_poti>\d)</a></td></tr>.*?'
+    r'<tr><td colspan="2"><p align="justify">(?P<Opis>.*?)</p></td></tr>.*?',
     flags=re.DOTALL
 )
 
 
 def podatki_izletov(izleti):
-    '''Sprejme seznam parov izletov (url, izlet), in naloži vsebino strani url kot niz'''
+    '''Sprejme seznam parov izletov (url, id), in naloži vsebino strani url kot niz'''
     podatki_izletov = []
-    for i in range(len(izleti)):
-        url = 'http://www.hribi.net/' + izleti[i][0]
-        nova_stran = 'stran_izlet_{}.html'.format(i)
-        stran = save_page(url, directory, nova_stran)
-        niz = read_file_to_string(directory, nova_stran)
+    for i in range(0, 3):
+        url_izleta = 'http://www.hribi.net/' + izleti[i][0]
+        stran_izleta = 'stran_izlet_{}.html'.format(izleti[i][1])
+        stran = save_page(url_izleta, ime_mape, stran_izleta)
+        niz = read_file_to_string(ime_mape, stran_izleta)
         podatki_izletov.append(re.findall(vzorec_izleta, niz)[0])
     return podatki_izletov
